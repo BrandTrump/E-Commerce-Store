dcd3ecffe8010bf8f10c5fdc28ccf7e66fbed256
Brandon Trump <brandontrump101@gmail.com>
2023-07-23 22:52:34 +1200

    feat: created footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="bg-white border-t mt-auto">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; 2023 Storename, Inc. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
