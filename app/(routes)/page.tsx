8f1cf27e10144aa850622a968ea2ce230a2221f3
Brandon Trump <brandontrump101@gmail.com>
2023-07-24 16:14:50 +1200

    feat: used helper function to fetch a billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/Billboard";
+import Container from "@/components/ui/Container";
+
+export const revalidate = 0;
+
+export default async function Home() {
+  const billboard = await getBillboard("5f5cde71-d85e-49e7-8343-44b66208ff83");
   return (
-    <main className="p-4">
-      <h1 className="text-xl">Home page</h1>
-    </main>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
   );
 }
