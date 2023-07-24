37cfe2034448157523eee1f2ec3ea53ebb796d57
Brandon Trump <brandontrump101@gmail.com>
2023-07-25 01:31:20 +1200

    feat: added function to handle displaying the modal

--- components/ui/ProductCard.tsx
+++ components/ui/ProductCard.tsx
@@ -6,6 +6,8 @@ import IconButton from "./IconButton";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./Currency";
 import { useRouter } from "next/navigation";
+import { MouseEventHandler } from "react";
+import usePreviewModal from "@/hooks/use-preview-modal";
 
 interface ProductCardProps {
   data: Product;
@@ -16,6 +18,14 @@ function ProductCard({ data }: ProductCardProps) {
   const handleClick = () => {
     router.push(`/product/${data?.id}`);
   };
+
+  const previewModal = usePreviewModal();
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    previewModal.onOpen(data);
+  };
+
   return (
     <article
       onClick={handleClick}
@@ -31,7 +41,7 @@ function ProductCard({ data }: ProductCardProps) {
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
             <IconButton
-              onClick={() => {}}
+              onClick={onPreview}
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
