1549333d69e95cadb80d81ee5654865cd667a4d9
Brandon Trump <brandontrump101@gmail.com>
2023-07-24 23:38:38 +1200

    feat: included the info component

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -3,6 +3,7 @@ import getProducts from "@/actions/get-products";
 import ProductList from "@/components/ProductList";
 import Gallery from "@/components/gallery";
 import Container from "@/components/ui/Container";
+import Info from "@/components/ui/Info";
 
 interface ProductPageProps {
   params: {
@@ -21,7 +22,9 @@ async function ProductPage({ params: { productId } }: ProductPageProps) {
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
             <Gallery images={product.images} />
-            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">Info</div>
+            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+              <Info data={product} />
+            </div>
           </div>
           <hr className="my-10" />
           <ProductList title="Related Items" items={suggestedProducts} />
