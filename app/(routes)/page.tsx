c5de935ddd8ec240d0a6e53f245ade110a1352f0
Brandon Trump <brandontrump101@gmail.com>
2023-07-24 16:38:08 +1200

    feat: used helper function to fetch products & imported product list component

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,15 +1,21 @@
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
 import Billboard from "@/components/Billboard";
+import ProductList from "@/components/ProductList";
 import Container from "@/components/ui/Container";
 
 export const revalidate = 0;
 
 export default async function Home() {
+  const products = await getProducts({ isFeatured: true });
   const billboard = await getBillboard("5f5cde71-d85e-49e7-8343-44b66208ff83");
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
       </div>
     </Container>
   );
