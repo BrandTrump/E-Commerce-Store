36672a077cef52025101324a36204790076025e8
Brandon Trump <brandontrump101@gmail.com>
2023-07-24 19:29:28 +1200

    feat: added an onClick event to route to a coresponding product page

--- components/ui/ProductCard.tsx
+++ components/ui/ProductCard.tsx
@@ -5,14 +5,22 @@ import Image from "next/image";
 import IconButton from "./IconButton";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./Currency";
+import { useRouter } from "next/navigation";
 
 interface ProductCardProps {
   data: Product;
 }
 
 function ProductCard({ data }: ProductCardProps) {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push(`/product/${data?.id}`);
+  };
   return (
-    <article className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <article
+      onClick={handleClick}
+      className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
+    >
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image
           src={data?.images?.[0]?.url}
