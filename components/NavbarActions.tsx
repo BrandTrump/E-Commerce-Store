998e7afc6e6ccc1c89e8a49409b0dda0f91df651
Brandon Trump <brandontrump101@gmail.com>
2023-07-26 11:50:24 +1200

    feat: route to cart page when clicking on cart icon

--- components/NavbarActions.tsx
+++ components/NavbarActions.tsx
@@ -4,6 +4,7 @@ import { ShoppingBag } from "lucide-react";
 import Button from "./ui/Button";
 import { useEffect, useState } from "react";
 import useCart from "@/hooks/use-cart";
+import { useRouter } from "next/navigation";
 
 function NavbarActions() {
   const [isMounted, setIsMounted] = useState(false);
@@ -12,13 +13,17 @@ function NavbarActions() {
     setIsMounted(true);
   }, []);
 
+  const router = useRouter();
   const cart = useCart();
 
   if (!isMounted) return;
 
   return (
     <div className="ml-auto flex items-center gap-x-4">
-      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+      <Button
+        className="flex items-center rounded-full bg-black px-4 py-2"
+        onClick={() => router.push("/cart")}
+      >
         <ShoppingBag size={20} color="white" />
         <span className="ml-2 text-sm font-medium text-white">
           {cart.items.length}
